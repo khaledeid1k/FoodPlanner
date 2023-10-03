2b64fbf53c0021dc047fd546cb4bee816b5b4814
khaledeid1k <khaled.eid1k1k@gmail.com>
2023-10-03 14:37:07 +0300

    fix move between fragment

--- app/src/main/java/com/example/foodplanner/MainActivity.java
+++ app/src/main/java/com/example/foodplanner/MainActivity.java
@@ -34,16 +34,4 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-//    @Override
-//    public boolean onSupportNavigateUp() {
-//        controller.navigateUp();
-//        return true;
-//    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-//        this.getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
-    }
 }
